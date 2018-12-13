8a6d181cac17f6a7282ae2f00cbabd913b95e9b5
PatrickGhadban <Pghadban@gmail.com>
2018-12-13 00:22:39 -0500

    Update baseGeneticAlgorithm.py
    fitness function now checks and adds pts to score for correct bomb locations (+2pts), even if the solution fails before reaching the end

--- minesweeper/baseGeneticAlgorithm.py
+++ minesweeper/baseGeneticAlgorithm.py
@@ -1,52 +1,48 @@
-import copy
-import time
+import baseGeneticAlgorithm
 import random
-from msgame import MSGame
+import time
+import copy
 
-class baseGeneticAlgorithm(object):
-    def __init__(self, boardWidth = 5, boardHeight = 3, bombs = 4, populationSize = 10, generationCount = 10, mutationRate = .05, crossoverRate = .75):
-        self.boardWidth = boardWidth
-        self.boardHeight = boardHeight
-        self.bombs = bombs
-        self.populationSize = populationSize
-        self.generationCount = generationCount
-        self.mutationRate = mutationRate
-        self.crossoverRate = crossoverRate
+LOSE = 0
+WIN = 1
+CONTINUE = 2
 
-        self.staticGame = MSGame(boardWidth, boardHeight, bombs)
+class basicGA(baseGeneticAlgorithm.baseGeneticAlgorithm):
+    def __init__(self, boardWidth = 5, boardHeight = 5, bombs = 2, populationSize = 100, generationCount = 100, crossoverRate = .75, mutationRate = .05):
+        super(basicGA,self).__init__(boardWidth,boardHeight,bombs,populationSize,generationCount,crossoverRate,mutationRate)
 
-        self.population = self.generatePopulation(populationSize, boardHeight * boardWidth, bombs)
-        self.setMaxFitness()
-
-    def generateChromosome(self, boardSize, bombCount):
-        ret = [0] * boardSize
-        for i in range(bombCount):
-            bomb_idx = random.randint(0, boardSize - 1)
-            while ret[bomb_idx] == 1:
-                bomb_idx = random.randint(0, boardSize - 1)
-            ret[bomb_idx] = 1
-        return ret
-
-    def generatePopulation(self, popSize, boardSize, bombCount):
-        ret = []
-        for i in range(popSize):
-            ret.append(self.generateChromosome(boardSize, bombCount))
-        return ret
-
-    def fitnessFunction(self, solution, board):
+    def fitnessFunction(self, solution):
         '''
-        Need to implement:
-            Takes in a particular solution string and a board to evaluate.
-            determines fitness of solution and returns fitness values
+        game = copy.deepcopy(self.staticGame)
+        score = 0
+        idx = 0
+        #Am I accessing list elements efficiently???
+        for i in range(self.boardWidth):
+            for j in range(self.boardHeight):
+                if solution[idx]:
+                    status = game.qplay('flag', i, j)
+                else:
+                    status = game.qplay('click', i, j)
+                    score += 1
+                if status == LOSE:
+                    return score - 1
+                elif status == WIN:
+                    return self.maxFitness
+                else:
+                    idx += 1            
+        return 0
         '''
         score, x_cord, y_cord = 0, 0, 0
 
+        # Iterates through solution and provides corresponding score
         for node in solution:
-            if node == board[y_cord][x_cord]:   # Node in chromosome matches the board
+            if node == self.board[y_cord][x_cord] and node == 0:   # Node in chromosome matches the board
                 score = score + 1
-            elif node == 0 and board[y_cord][x_cord] == 1:    # Mistakes empty tile for bomb tile - loss
-                return score
-            elif node == 1 and board[y_cord][x_cord] == 0:   # Mistakes bomb tile for empty tile - minue 1pt
+            elif node == self.board[y_cord][x_cord] and node == 1:   # Node in chromosome matches the board
+                score = score + 2
+            elif node == 0 and self.board[y_cord][x_cord] == 1:    # Mistakes empty tile for bomb tile - loss
+                return score + self.checkBombLocation(solution)
+            elif node == 1 and self.board[y_cord][x_cord] == 0:   # Mistakes bomb tile for empty tile - minus 1pt
                 score = score - 1
 
             # Iterates to the next coordinate on the board
@@ -55,79 +51,90 @@ class baseGeneticAlgorithm(object):
             elif y_cord < self.boardHeight - 1:
                 x_cord = 0
                 y_cord = y_cord + 1
+        return score
 
-    def getFitnessVals(self):
-        ret = []
-        for chromosome in self.population:
-            ret.append(self.fitnessFunction(chromosome, self.staticGame.get_mine_map()))
-        return ret
+    def checkBombLocation(self, solution):
+        score, x_cord, y_cord = 0, 0, 0
+        for node in solution:
+            if node == 1:
+                if node == self.board[y_cord][x_cord]:
+                    score = score + 2
+            if x_cord < self.boardWidth - 1:
+                x_cord = x_cord + 1
+            elif y_cord < self.boardHeight - 1:
+                x_cord = 0
+                y_cord = y_cord + 1
+        return score
 
     def setMaxFitness(self):
-        '''
-        Needs to be implemented:
-            Needs to set maximum fitness value according to whatever fitness algorithm is being used
-        Score will be the length of the chromosome because +1 is given for each correct guess - PG Edit
-        '''
-        #self.maxFitness = float('inf')
-        self.maxFitness = self.boardWidth * self.boardHeight
-        return self.maxFitness
+        self.maxFitness = self.boardHeight * self.boardWidth + self.bombs
 
-    def parentalSelection(self, fitPopulation):
-        while 1:
-            p1 = self.tournament(fitPopulation)
-            p2 = self.tournament(fitPopulation)
-            return (p1, p2)
-
-    def tournament(self, fitPopulation):
-        fit1, ch1 = fitPopulation[random.randint(0, len(fitPopulation) - 1)]
-        fit2, ch2 = fitPopulation[random.randint(0, len(fitPopulation) - 1)]
-
-        return ch1 if fit1 > fit2 else ch2
-
-    def recombinationAlg(self, tupleList):
+    def parentSelection(self, sortedTuples):
         '''
         Need to implement:
-            Takes in a list of tuples of form (chromosome, fitness).
-            returns a list of chromosomes representing the newly recombined pop
+            Takes in list of tuples of form (chromosome, fitness)
+            returns tuple of form (parentChromosome1, parentChromosome2)
         '''
-        pop, fitness = zip(*tupleList)
-        return pop
+        parList = []
+        for i in range(5):
+            parList.append(sortedTuples[random.randint(0,self.populationSize - 1)])
+        parent1 = max(parList, key=lambda item:item[1])
+        parList.remove(parent1)
+        parent2 = max(parList, key=lambda item:item[1])
+        return (parent1[0],parent2[0])
+
+    def crossoverAlg(self, parents):
+        '''
+        Need to implement:
+            Takes in a tuple of form (parentChromosome1, parentChromosome2)
+            Returns a tuple of form (childChromsome1, childChromsome2)
+        '''
+        pars = copy.copy(parents)
+        parent1 = pars[0]
+        parent2 = pars[1]
+
+        usedPoints = set()
+        for i in range(self.bombs // 2):
+            crossPoint = random.randint(0, self.bombs - 1)
+            while crossPoint in usedPoints:
+                crossPoint = random.randint(0, self.bombs - 1)
+            usedPoints.add(crossPoint)
+
+            old_idx_1 = (parent1[1])[crossPoint]
+            old_idx_2 = (parent2[1])[crossPoint]
+            if (parent1[0])[old_idx_2] == 0:
+                (parent1[0])[old_idx_1] = 0
+                (parent1[1])[crossPoint] = old_idx_2
+                (parent1[0])[old_idx_2] = 1
+
+            if (parent2[0])[old_idx_1] == 0:
+                (parent2[0])[old_idx_2] = 0
+                (parent2[1])[crossPoint] = old_idx_1
+                (parent2[0])[old_idx_1] = 1
+
+        return (parent1,parent2)
 
     def mutationAlg(self):
         '''
         Need to implement:
             Updates current population according in desired manner
         '''
-        pass
 
-    def getMaxChromosome(self, tupleList):
-        maxVal = None
-        for item in tupleList:
-            if maxVal is None or maxVal < item[1]:
-                maxChromosome = item[0]
-                maxVal = item[1]
-        return maxChromosome
+        threshhold = self.mutationRate * 100
+        for ch in self.population:
+            for i in range(len(ch[1])):
+                if random.randint(0,100) < threshhold:
+                    cur_idx = (ch[1])[i]
+                    new_idx = random.randint(0,len(ch[0]) - 1)
+                    if (ch[0])[new_idx] != 1:
+                        (ch[0])[new_idx] = 1
+                        (ch[0])[cur_idx] = 0
+                        (ch[1])[i] = new_idx
 
-    def runEpoch(self):
-        finalChromosome = None
-        for generation_idx in range(self.generationCount):
-            fitnesses = self.getFitnessVals()
-            print ("\n")
-            print(fitnesses)
-            print ("\n")
 
-            1/0
-            popFitness = list(zip(self.population, fitnesses))
 
-            if self.maxFitness == max(fitnesses):
-                break
-            population = self.recombinationAlg(popFitness)
-            self.mutationAlg()
-        finalChromosome = self.getMaxChromosome(popFitness)
-        return finalChromosome
+
 
 if __name__ == '__main__':
     random.seed(time.time())
-    AI = baseGeneticAlgorithm()
-    AI.runEpoch()
-    #import code; code.interact(banner='', local = locals())
+    import code; code.interact(banner='', local = locals())
